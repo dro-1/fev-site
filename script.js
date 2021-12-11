d7b3d9c5b2f1a192d9d2c7aa05b4ed85fd370a2a
Seun Taiwo <lorddro1532@gmail.com>
2021-12-11 17:36:08 +0100

    Finished mobile of homepage

--- script.js
+++ script.js
@@ -3,14 +3,32 @@ const closeBtn = document.querySelector(".close");
 const mobileNav = document.querySelector(".mobile-nav");
 const header = document.querySelector("header");
 
+const serviceLink = document.querySelector(".mobile-nav ul li:nth-child(1) a");
+
+const serviceDiv = document.querySelector("section.services");
+
+let isMenuOpen = false;
+
+const switchNav = (type) => {
+  if (type === "open") {
+    mobileNav.classList.add("active");
+    hamburgerBtn.firstElementChild.src = `${window.location.origin}/assets/svg/cancel.svg`;
+    hamburgerBtn.firstElementChild.classList.add("small");
+    isMenuOpen = true;
+  } else {
+    mobileNav.classList.remove("active");
+    hamburgerBtn.firstElementChild.src = `${window.location.origin}/assets/svg/menu.svg`;
+    hamburgerBtn.firstElementChild.classList.remove("small");
+    isMenuOpen = false;
+  }
+};
 hamburgerBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  mobileNav.classList.add("active");
-});
-
-closeBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  mobileNav.classList.remove("active");
+  if (!isMenuOpen) {
+    switchNav("open");
+  } else {
+    switchNav("close");
+  }
 });
 
 window.addEventListener("scroll", () => {
@@ -18,7 +36,17 @@ window.addEventListener("scroll", () => {
     header.classList.add("active");
   } else {
     header.classList.remove("active");
+    switchNav("close");
   }
+});
+
+serviceLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: serviceDiv.offsetTop - 40,
+    behavior: "smooth",
+  });
+  switchNav("close");
 });
 
 $(document).ready(function () {
