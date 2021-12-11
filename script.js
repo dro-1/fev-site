603418d978e08a55a2c139f38edb698e0730b9e5
Seun Taiwo <lorddro1532@gmail.com>
2021-12-11 13:44:03 +0100

    Almost done with homepage

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburgerBtn = document.querySelector(".hamburger");
 const closeBtn = document.querySelector(".close");
 const mobileNav = document.querySelector(".mobile-nav");
+const header = document.querySelector("header");
 
 hamburgerBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -10,4 +11,18 @@ hamburgerBtn.addEventListener("click", (e) => {
 closeBtn.addEventListener("click", (e) => {
   e.preventDefault();
   mobileNav.classList.remove("active");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 152) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+$(document).ready(function () {
+  $(".carousel").slick({
+    dots: true,
+  });
 });
